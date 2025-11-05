489e00718f1b55c2fc48c9a207c37dbdf412f5f1
Michayel <michael.navoyan@ispras.ru>
2025-11-05 13:02:37 +0400

    feat: add Tracker class

---
+++ simple_tracker/tracker.py
@@ -0,0 +1,16 @@
+class Tracker:
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+    
+    def save_to_file(self):
+        with open("data.txt", "w") as f:
+            f.write(str(self.count))
+    
+    def reset(self):  
+        self.count = 0  
+          
+    def __str__(self):
+        return f"Current count is: {self.count}"
